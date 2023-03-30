a86809cebd3a32d3a30bd82c2eb406fcf243ec6f
Aluno <kaua.moreno2005@gmail.com>
2023-03-30 10:14:40 -0300

    iniciando o backend express - localhost:3000

---
+++ src/express.js
@@ -0,0 +1,21 @@
+// Importar o modulo express
+const express = require('express');
+
+// Criando uma instancia
+const app = express();
+
+// Definindo a porta do servidor
+const port = 3000;
+
+// Função para ser executada em cada requisição feita ao servidor
+app.use(express.static('./public'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+// Exibindo a mensagem no console
+console.log(`A porta esta conectada no localhost:${port}`);
+
+// Iniciando o servidor na porta definida
+app.listen(port)
